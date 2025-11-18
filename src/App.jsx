2dde19a265a2bdbed5505403a813b1f61246107c
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 18:07:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { BrowserRoutehere
-        Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Vendors from './pages/Vendors';
-import Products from './pages/Products';
-import Orders from './pages/Orders';
-import Login from './pages/Login';
-import { AuthProvider } from './context/AuthContext';
-import Sidebar from './components/Sidebar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
-function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <Sidebar />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/vendors" element={<Vendors />} />
-                    <Route path="/products" element={<Products />} />
-                    <Route path="/orders" element={<Orders />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
