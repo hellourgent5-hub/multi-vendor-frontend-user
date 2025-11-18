638c417973b9fe59eded6b931152c2dcb3b0f05f
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 11:29:29 +0530

    Create ProtectedRoute.jsx

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "../api";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then(res => setUser(res.data))
+      .catch(() => setUser(null))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" />;
+
+  return children;
+}
