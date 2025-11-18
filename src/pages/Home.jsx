bd929c919357685a4503cc5084cc26a0b1ce0963
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 11:19:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,28 @@
-import React from 'react';
-export default function Home(){ return (<div><h1>Welcome</h1><p>Multi-vendor marketplace</p></div>); }
+import { useEffect, useState } from "react";
+import API from "../api";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    API.get("/products") // Make sure backend has /products route
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.length === 0 && <p>No products available</p>}
+      <ul>
+        {products.map(prod => (
+          <li key={prod._id}>
+            {prod.name} - ${prod.price}
+            <Link to={`/cart`}>Add to Cart</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
