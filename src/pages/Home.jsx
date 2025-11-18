89d3d90bb70ac62f539302fa1fe9387741b9c1b5
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 11:38:31 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,35 @@
 import { useEffect, useState } from "react";
-import API from "../api";
+import { getProducts, addToCartAPI } from "../api";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    try {
-      const res = await API.get("/products");
-      setProducts(res.data);
-    } catch (err) {
-      console.error(err.response?.data || err.message);
-    }
-  };
+  useEffect(() => {
+    getProducts()
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
 
-  const addToCart = async (productId) => {
+  const addToCart = async (id) => {
     try {
-      await API.post("/cart", { productId });
+      await addToCartAPI(id);
       alert("Added to cart!");
     } catch (err) {
       alert("Failed to add to cart");
-      console.error(err.response?.data || err.message);
+      console.error(err);
     }
   };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
 
   return (
     <div>
       <h2>Products</h2>
       {products.length === 0 && <p>No products available</p>}
       <ul>
-        {products.map((prod) => (
-          <li key={prod._id}>
-            {prod.name} - ${prod.price}
-            <button onClick={() => addToCart(prod._id)}>Add to Cart</button>
+        {products.map(p => (
+          <li key={p._id}>
+            {p.name} - ${p.price} 
+            <button onClick={() => addToCart(p._id)}>Add to Cart</button>
           </li>
         ))}
       </ul>
