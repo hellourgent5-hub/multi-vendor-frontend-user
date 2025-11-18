6da2e7aa2a9d3594be7763a8a7c19071a8b573ae
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 23:47:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar.jsx";
+
 import Home from "./pages/Home.jsx";
 import LoginRegister from "./pages/LoginRegister.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
+
 import CartPage from "./pages/CartPage.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import Orders from "./pages/Orders.jsx";
+
 import { AuthProvider } from "./context/AuthContext.jsx";
-import Cart from "./pages/Cart.jsx";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginRegister />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/cart" element={<Cart />} /> 
-          <Route path="/cart" element={<Cart />} />
 
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
+
+          {/* Auth */}
+          <Route path="/login" element={<LoginRegister />} />
+
+          {/* Products */}
+          <Route path="/product/:id" element={<ProductDetails />} />
+
+          {/* Cart */}
+          <Route path="/cart" element={<CartPage />} />
+
+          {/* Checkout */}
+          <Route path="/checkout" element={<Checkout />} />
+
+          {/* Orders */}
+          <Route path="/orders" element={<Orders />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
