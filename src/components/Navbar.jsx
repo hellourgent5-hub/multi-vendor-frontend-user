afbc3a9f702846912df63bcbeba3b3749fee5cd6
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 18:16:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function Navbar(){
+export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/products">Products</Link> | <Link to="/cart">Cart</Link> | <Link to="/profile">Profile</Link>
+    <nav className="p-4 bg-black text-white flex justify-between">
+      <Link to="/">Home</Link>
+      <div className="flex gap-4">
+        <Link to="/cart">Cart</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/login">Login</Link>
+      </div>
     </nav>
   );
 }
