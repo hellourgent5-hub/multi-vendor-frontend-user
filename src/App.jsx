338d50b26c90ef32786d4f358e5b4afd41df28f8
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 07:47:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar.jsx";
-
 import Home from "./pages/Home.jsx";
 import LoginRegister from "./pages/LoginRegister.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
-
 import CartPage from "./pages/CartPage.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import Orders from "./pages/Orders.jsx";
-
 import { AuthProvider } from "./context/AuthContext.jsx";
+import Categories from "./pages/Categories.jsx";
+import CategoryProducts from "./pages/CategoryProducts.jsx";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
-          {/* Home */}
           <Route path="/" element={<Home />} />
-
-          {/* Auth */}
           <Route path="/login" element={<LoginRegister />} />
-
-          {/* Products */}
           <Route path="/product/:id" element={<ProductDetails />} />
-
-          {/* Cart */}
           <Route path="/cart" element={<CartPage />} />
-
-          {/* Checkout */}
           <Route path="/checkout" element={<Checkout />} />
-
-          {/* Orders */}
           <Route path="/orders" element={<Orders />} />
+
+          {/* NEW */}
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/category/:name" element={<CategoryProducts />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
